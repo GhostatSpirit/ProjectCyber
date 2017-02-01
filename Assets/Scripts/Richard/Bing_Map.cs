6b0ccc321f64b4a59183e8a3334f631bc5562335
Yang Liu <lykavin@hotmail.com>
2017-02-01 23:25:13 +0800

    Added files from the previous repo. God please make this work.

---
+++ Assets/Scripts/Richard/Bing_Map.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Bing_Map : MonoBehaviour {
+    RawImage rawImage;
+    public Bing_Map_Location query_data;
+    public int zoom = 13;
+    public int size = 512;
+    public string bing_map_key = "AkgiM7JUZs4i22iTTzRUZfBfLr3N_GbeMShMEk0f2W6AeQfbMlV-5OfwvS8zIcyo";
+
+    // Use this for initialization
+    void Start()
+    {
+        rawImage = GetComponent<RawImage>();
+        refresh();
+    }
+
+    public void refresh()
+    {
+        StartCoroutine(get());
+    }
+
+    public IEnumerator get()
+    {
+        string url = "http://dev.virtualearth.net/REST/v1/Imagery/Map/Road";
+        string center_location = string.Format("{0},{1}", query_data.latitude, query_data.longitude);
+        string u_zoom = zoom.ToString();
+        string map_size = string.Format("mapSize={0},{1}", query_data.size_X, query_data.size_Y);
+        string key = string.Format("&key={0}", bing_map_key).Replace(" ","");
+        var final_url = new WWW(url + "/" + center_location + "/" + u_zoom + "?" + map_size + key);
+        yield return final_url;
+        rawImage.texture = final_url.textureNonReadable;
+        rawImage.SetNativeSize();
+    }
+	
+    [System.Serializable]
+    public class Bing_Map_Location
+    {
+        public double latitude = 35.7022121;
+        public double longitude = 139.7722703;
+        public int size_X = 600;
+        public int size_Y = 600;
+    }
+
+}
---
+++ Assets/Scripts/Yang/PlayerAim.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class PlayerAim : MonoBehaviour {
+	// replaced with InControl
+	//public string horizontalAxisName = "Horizontal";
+	//public string verticalAxisName = "Vertical";
+
+	public int playerIndex = 0;
+	public Transform deviceAssigner;
+	InputDevice myInputDevice;
+
+	Vector2 moveVector;
+	public float radialDeadZone = 0.1f;
+
+	float angularVelocity = 8f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myInputDevice = deviceAssigner.
+			GetComponent<DeviceAssigner>().GetPlayerDevice(playerIndex);
+
+		if(myInputDevice == null){
+			return;
+		}
+		// get the axis values, construct a vector and normalize it
+		float horizontal = myInputDevice.RightStickX;
+		float vertical = myInputDevice.RightStickY;
+
+		moveVector = new Vector3(horizontal, vertical, 0f);
+
+
+		if (moveVector.magnitude > radialDeadZone) {
+			var currentRot = Quaternion.LookRotation (Vector3.forward, moveVector);
+			transform.rotation = Quaternion.Lerp (transform.rotation, currentRot,
+				Time.deltaTime * angularVelocity);
+		}
+//		if(moveVector.magnitude != 0f){
+//			transform.up = moveVector;
+//		}
+	}
+}
