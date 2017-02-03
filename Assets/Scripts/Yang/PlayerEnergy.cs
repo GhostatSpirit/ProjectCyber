07e9474b18904c9f6ee7a20c88bc2bcf50196007
Yang Liu <lykavin@hotmail.com>
2017-02-03 12:20:13 +0800

    New Commit without InControl.

---
+++ Assets/Scripts/Yang/PlayerEnergy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEnergy : MonoBehaviour {
+	public float initialEnergy = 100f;
+	public float maxEnergy = 100f;
+
+	float playerEnergy;
+	// Use this for initialization
+	void Start () {
+		playerEnergy = initialEnergy;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// subtract the used energy and return the remaining energy
+	public float UseEnergy(float deltaEnergy){
+		playerEnergy -= deltaEnergy;
+		if(deltaEnergy > 0f){
+			deltaEnergy = 0f;
+		}
+		return playerEnergy;
+	}
+
+	// add the additional energy and return the remaining energy
+	public float AddEnergy(float deltaEnergy){
+		playerEnergy += deltaEnergy;
+		if(playerEnergy > maxEnergy){
+			playerEnergy = maxEnergy;
+		}
+		return playerEnergy;
+	}
+
+	public float GetEnergy(float deltaEnergy){
+		return playerEnergy;
+	}
+
+	public bool IsDepleted(){
+		return (playerEnergy == 0f);
+	}
+}
