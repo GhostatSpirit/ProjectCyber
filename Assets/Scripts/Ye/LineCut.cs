aa6b81684ba3327067ac19542200c430f94ba61c
Ye Wu <borderwing@sjtu.edu.cn>
2017-02-08 18:31:27 +0800

    2017/2/8 LineCut updated

--- Assets/Scripts/Ye/LineCut.cs
+++ Assets/Scripts/Ye/LineCut.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LineCut : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other){
         Debug.Log("Object caught");
-        if (other.gameObject.tag == "Player1")
-		    Destroy (gameObject);
+        if (other.gameObject.tag == "EnemyLine")
+		    Destroy (other.gameObject);
 	}
 }
