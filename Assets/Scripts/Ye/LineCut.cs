3f8334bf8f0292ba4ed0986b75c35c8fe66ab3c2
Ye Wu <borderwing@sjtu.edu.cn>
2017-02-15 20:24:06 +0800

    2017/2/15 LineCut updated : GameObject myLine added , not destroyed.

--- Assets/Scripts/Ye/LineCut.cs
+++ Assets/Scripts/Ye/LineCut.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 
 public class LineCut : MonoBehaviour {
 
+    Rigidbody2D AIrb;
+    Vector2 VAI;
+    Vector2 AIPos;
+    Vector2 AIPos1;
+    Vector2 LineStart;
+    Vector2 LineEnd;
+    Vector2 Hit;
 
-    void OnCollisionEnter2D(Collision2D collision)
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        Vector3 CollisionPoint;
-        CollisionPoint = collision.contacts[0].point;
-        print(CollisionPoint);
-        Debug.Log(CollisionPoint);
-
-    }
-
-    void OnTriggerEnter2D(Collider2D other){
-        Invoke("OnCollisionEnter2D", 0.00001f);
         if (other.gameObject.tag == "EnemyLine")
-            Debug.Log("Object caught");
-		    Destroy (other.gameObject);
-	}
-
+        {
+            AIrb = GetComponent<Rigidbody2D>();
+            AIPos = AIrb.position;
+            VAI = AIrb.velocity;
+            LineStart = other.GetComponent<LineRenderer>().GetPosition(0);
+            LineEnd = other.GetComponent<LineRenderer>().GetPosition(1);
+            AIPos1 = new Vector2 ( VAI.x+AIPos.x , VAI.y + AIPos.y);
+            float D = (AIPos1.x - AIPos.x) * (LineEnd.y - LineStart.y)-(LineEnd.x - LineStart.x)*(AIPos1.y - AIPos.y);
+            float b1 = (AIPos1.y - AIPos.y) * AIPos.x + (AIPos.x - AIPos1.x) * AIPos.y;
+            float b2 = (LineEnd.y - LineStart.y) * LineStart.x + (LineStart.x - LineEnd.x) * LineStart.y; ;
+            float D1 = b2 * (AIPos1.x - AIPos.x) - b1 * (LineEnd.x - LineStart.x);
+            float D2 = b2 * (AIPos1.y - AIPos.y) - b1 * (LineEnd.y - LineStart.y);
+            float X0 = D1 / D;
+            float Y0 = D2 / D;
+            Hit = new Vector2(X0, Y0);
+            Debug.Log("Hit");
+            Debug.Log(X0);
+            Debug.Log(Y0);
+            Destroy(other.gameObject);
+        }
+    }
 }
--- Assets/Scripts/Ye/LineHack.cs
+++ Assets/Scripts/Ye/LineHack.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using InControl;
 
-public class LineHack : MonoBehaviour {
+public class LineHack : MonoBehaviour
+{
+
+    InputDevice myInputDevice;
     static GameObject myLine;
+
+
     GameObject FindClosestEnemy()
     {
         GameObject[] gos;
@@ -23,37 +29,82 @@ public class LineHack : MonoBehaviour {
         }
         return closest;
     }
-
     void Show()
     {
         Debug.Log(FindClosestEnemy());
         Debug.Log("Showed");
     }
 
-    void DrawLine(Vector3 start, Vector3 end, float duration = 0.2f)
+    void Start()
     {
         myLine = new GameObject();
-        myLine.transform.position = start;
+        myLine.transform.position = gameObject.transform.position;
         myLine.AddComponent<LineRenderer>();
         LineRenderer lr = myLine.GetComponent<LineRenderer>();
         lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
-        Color color = Color.red;
-        lr.SetWidth(0.1f, 0.1f);
-        lr.SetColors(color,color);
+        Color color = Color.clear;
+        lr.startWidth = 0.05f;
+        lr.endWidth = 0.05f;
+        lr.startColor = color;
+        lr.endColor = color;
         lr.SetPosition(0, gameObject.transform.position);
         lr.SetPosition(1, FindClosestEnemy().transform.position);
-       // Debug.Log("Line is drawn");
+
     }
+
+    void Draw(GameObject start , GameObject end )
+    {
+        LineRenderer lr= myLine.GetComponent<LineRenderer>();
+        Color color = Color.red; 
+        lr.startColor = color;
+        lr.endColor = color;
+        lr.SetPosition(0, start.transform.position);
+        lr.SetPosition(1, end.transform.position);
+    } 
+    void Clean()
+    {
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        Color color = Color.clear;
+        lr.startColor = color;
+        lr.endColor = color;
+    }
+    
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.G))
-        {
-            Destroy(myLine, 0.0001f);
-            //Invoke("Show",1f);
-            DrawLine(gameObject.transform.position, FindClosestEnemy().transform.position);
-        }
-   
-    }
+        myInputDevice = GetComponent<DeviceReceiver>().GetDevice();
 
+        if (myInputDevice == null)
+        {
+            return;
+        }
+        float horizontal = myInputDevice.LeftStickX;
+        float vertical = myInputDevice.LeftStickY;
+
+        if (myInputDevice.RightTrigger.IsPressed == true )
+        {
+            // Destroy(myLine, 0.0000001f);
+            // Invoke("Show",1f);
+            Draw(gameObject, FindClosestEnemy());
+           
+        }
+
+        
+        if (myInputDevice.RightTrigger.WasReleased== true)
+        {
+            Clean();
+        }
+        
+
+        /*/
+                if (Input.GetKey(KeyCode.G))
+                {
+                    Destroy(myLine, 0.0001f);
+                    //Invoke("Show",1f);
+                    DrawLine(gameObject.transform.position, FindClosestEnemy().transform.position);
+                }
+
+            
+        /*/
+    }
 }
