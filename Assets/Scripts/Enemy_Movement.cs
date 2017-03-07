0a9aa9454d30eb0ffbc5789c0d36c669ff2d912b
RichardLuo79 <richardluo723@gmail.com>
2017-03-07 22:12:34 +0800

    Manual Update

---
+++ Assets/Scripts/Enemy_Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(36, 36);
+    private Vector2 movement;
+    public GameObject target;
+    private Rigidbody2D rb;
+    private RaycastHit2D rch;
+    private LayerMask lm;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        speed = (target.transform.position - transform.position).normalized;
+        Vector2 start_pos = transform.position + new Vector3(speed.normalized.x * 5, speed.normalized.y * 5, 0);
+        Vector2 end_pos = transform.position + new Vector3(speed.normalized.x * 14, speed.normalized.y * 14, 0);
+        Debug.DrawLine(start_pos, end_pos, Color.red);
+        movement = new Vector2(speed.x * 28f, speed.y * 28f);
+
+        rch = Physics2D.Linecast(start_pos, end_pos, 1);
+        if (rch.collider == null)
+            rb.velocity = movement;
+        else
+        {
+            Debug.Log("Hit");
+            rb.velocity = new Vector2(-movement.x, -movement.y);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/Yang/FOWGridControl.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FOWGridControl : MonoBehaviour {
+	SpriteRenderer gridRenderer;
+	public float fadeSpeed = 5f;
+
+	bool startFading = false;
+
+	void Start(){
+		gridRenderer = GetComponent<SpriteRenderer> ();
+	}
+
+	void Update(){
+		if(startFading){
+			Color color = gridRenderer.material.color;
+			color.a -= fadeSpeed / 10f * Time.deltaTime;
+			if(color.a <= 0f){
+				color.a = 0f;
+				Destroy (transform.gameObject);
+			}
+			gridRenderer.material.color = color;
+		}
+	}
+
+
+	// Replaced for performance concerns
+//	void OnTriggerStay2D( Collider2D activator ) {
+//		if ( activator.gameObject.tag == "PlayerSight" ) {
+//			startFading = true;
+//			Color color = gridRenderer.material.color;
+//			color.a -= fadeSpeed / 10f * Time.deltaTime;
+//			if(color.a <= 0f){
+//				color.a = 0f;
+//				Destroy (transform.gameObject);
+//			}
+//			gridRenderer.material.color = color;
+//		}
+//	}
+
+	// a function that is automatically called when
+	// something with a Rigidbody2D enters this trigger
+	void OnTriggerEnter2D( Collider2D activator ) {
+		// Is player activating the trigger?
+		if ( activator.gameObject.tag == "PlayerSight" ) {
+			startFading = true;
+		}
+	}
+}
