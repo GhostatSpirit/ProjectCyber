7aa2091ebe38dba1292afe81a64839d0790b62cd
Ye Wu <borderwing@sjtu.edu.cn>
2017-04-08 12:58:24 +0800

    testPortal added

---
+++ Assets/Scripts/Ye/NaiveTransition.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Com.LuisPedroFonseca.ProCamera2D;
+using UnityEngine;
+
+public class NaiveTransition : MonoBehaviour {
+
+    public Camera cam;
+    public GameObject target;
+    public GameObject initial;
+
+    // Use this for initialization
+    void Start () {
+        ProCamera2DTransitionsFX s = cam.GetComponent<ProCamera2DTransitionsFX>();
+        //StartCoroutine(DelayedOperations());
+        //print("start coroutine");
+    }
+
+    IEnumerator DelayedOperations()
+    {
+        ProCamera2DTransitionsFX s = cam.GetComponent<ProCamera2DTransitionsFX>();
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //s.TransitionExit();
+            s.TransitionExit();
+            yield return new WaitForSeconds(1.5f);
+            gameObject.transform.position = target.transform.position;
+            yield return new WaitForSeconds(1f);
+            s.TransitionEnter();
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            gameObject.transform.position = initial.transform.position;
+        }
+        
+    }
+    void Update()
+    {
+        StartCoroutine(DelayedOperations());
+    }
+    // Update is called once per frame
+    /*
+	void Update () {
+         ProCamera2DTransitionsFX s = cam.GetComponent<ProCamera2DTransitionsFX>();
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //s.TransitionExit();
+            s.TransitionExit();
+            
+            gameObject.transform.position = target.transform.position;
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            gameObject.transform.position = initial.transform.position;
+        }
+
+    }
+    */
+
+
+}
---
+++ Assets/Scripts/Ye/TestPortal.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Com.LuisPedroFonseca.ProCamera2D;
+
+public class TestPortal : MonoBehaviour {
+
+    public Camera cam;
+    public GameObject AI;
+    public GameObject Hacker;
+    public float MaxtransTime;
+    public GameObject destination;
+
+    public float transTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+        ProCamera2DTransitionsFX transFX = cam.GetComponent<ProCamera2DTransitionsFX>();
+        Collider2D portalcol = gameObject.GetComponent<CircleCollider2D>();
+        Collider2D AIcol = AI.GetComponent<CircleCollider2D>();
+        Collider2D Hackercol = Hacker.GetComponent<CircleCollider2D>();  
+	}
+
+    // Update is called once per frame
+
+    IEnumerator DelayedTransition(ProCamera2DTransitionsFX transFX)
+    {
+        transFX.TransitionExit();
+        yield return new WaitForSeconds(0.5f);
+        AI.transform.position = destination.transform.position;
+        Hacker.transform.position = destination.transform.position;
+        yield return new WaitForSeconds(1f);
+        transFX.TransitionEnter();
+    }
+    
+
+	void Update () {
+        ProCamera2DTransitionsFX transFX = cam.GetComponent<ProCamera2DTransitionsFX>();
+        Collider2D portalcol = gameObject.GetComponent<CircleCollider2D>();
+        Collider2D AIcol = AI.GetComponent<CircleCollider2D>();
+        Collider2D Hackercol = Hacker.GetComponent<CircleCollider2D>();
+        
+        //  Examine whether ai and hacker are in the portal 
+        if (portalcol.IsTouching(AIcol) && portalcol.IsTouching(Hackercol))
+        {
+            transTime += Time.deltaTime;
+        }
+
+        //  If not set transTime to 0
+        else
+        {
+            transTime = 0f;
+        }
+
+        //  If transTime >= MaxtransTime, transport
+        if (transTime >= MaxtransTime)
+        {
+            transTime = 0f;
+            StartCoroutine(DelayedTransition(transFX));
+        }
+
+    }
+}
