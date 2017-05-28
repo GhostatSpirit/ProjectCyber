909cac94eca000dff8d867ed58e788f7c6b5653e
Ye Wu <borderwing@sjtu.edu.cn>
2017-05-29 03:17:47 +0800

    rebot anim added; Energy To Light added

---
+++ Assets/Scripts/Ye/EnergyToLight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyToLight : MonoBehaviour {
+
+
+    SFLight sflight;
+    PlayerEnergy pe;
+    public float minInt;
+    public float maxInt;
+
+	// Use this for initialization
+	void Start () {
+        sflight = GetComponent<SFLight>();
+        pe = GetComponentInParent<PlayerEnergy>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        sflight.intensity = (float)(minInt + ( maxInt - minInt ) * ( pe.GetEnergy() / pe.maxEnergy ));
+	}
+}
