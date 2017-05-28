09dbcb4b7a66d438b51acbd6d4b8fa6f62662093
Yang Liu <lykavin@hotmail.com>
2017-05-28 14:33:24 +0800

    Half-implemented new laser room.

---
+++ Assets/Scripts/Yang/DieTogether.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieTogether : MonoBehaviour {
+
+	HealthSystem[] healths;
+
+	bool killedAll = false;
+	// Use this for initialization
+	void Start () {
+		healths = GetComponentsInChildren<HealthSystem> ();
+		foreach(HealthSystem hs in healths){
+			hs.OnObjectDead += KillAll;
+		}
+	}
+
+	void KillAll(Transform objTrans){
+		if(killedAll){
+			return;
+		}
+		foreach(HealthSystem hs in healths){
+			if(hs.transform != objTrans){
+				hs.InstantDead ();
+				hs.OnObjectDead -= KillAll;
+			} else{
+				hs.OnObjectDead -= KillAll;
+			}
+		}
+		killedAll = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
