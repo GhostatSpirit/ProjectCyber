abac85972315cc0ca5f3375dd2e7260895a4fcde
Yang Liu <lykavin@hotmail.com>
2017-06-22 11:33:57 +0800

    Improved hacker aim cursor. Added boss explosion effect.

---
+++ Assets/Scripts/Yang/PlayerBehavior/AimCursorRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimCursorRotation : MonoBehaviour {
+
+	public Transform shootDummy;
+
+	Quaternion viewAngleRot;
+
+	// Use this for initialization
+	void Start () {
+		viewAngleRot = Quaternion.AngleAxis (30f, Vector3.right);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion shootRot = shootDummy.rotation;
+		Quaternion finalRot =  viewAngleRot * shootRot;
+		transform.rotation = finalRot;
+	}
+}
